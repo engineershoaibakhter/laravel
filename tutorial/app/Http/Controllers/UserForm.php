4a08229cc9ecc194b1c2e4dfbf38c4dcab788dee
engineershoaibakhter <shoaibakhter181422@gmail.com>
2026-02-27 12:02:34 +0500

    feat: learn validation and custom validation with custom rules

--- tutorial/app/Http/Controllers/UserForm.php
+++ tutorial/app/Http/Controllers/UserForm.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class UserForm extends Controller
 {
+
     function getFormData(Request $request){
+        $request->validate([
+            'username'=>'required | min:3',
+            'email'=>'required | email',
+            'password'=>'required | max:8'
+        ],[
+            'username.required'=>'Username is required field',
+            'username.uppercase'=>'Username must be in uppercase',
+        ]);
         echo $request;
     }
 }
--- tutorial/resources/views/user-form.blade.php
+++ tutorial/resources/views/user-form.blade.php
@@ -1,9 +1,12 @@
 <div>
-    <form action="addUser" method="POST">
+    <form action="addUser" method="POST" style="text-align: center; margin-top: 100px;">
         @csrf
-        <input type="text" name="username" placeholder="username"> <br>
-        <input type="email" name="email" placeholder="email"> <br>
-        <input type="password" name="password" placeholder="password"> <br>
+        <input type="text" name="username" placeholder="username" value="{{old('username')}}"> <br> <br>
+        <span style="color: red">@error('username') {{$message}} @enderror</span><br> <br>
+        <input type="email" name="email" placeholder="email" value="{{old('email')}}"> <br><br>
+        <span style="color: red">@error('email') {{$message}} @enderror</span><br> <br>
+        <input type="password" name="password" placeholder="password" value="{{old('password')}}"> <br><br>
+        <span style="color: red">@error('password') {{$message}} @enderror</span><br> <br>
         <button type="submit">Submit</button>
     </form>
 </div>
