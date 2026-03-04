b9a412f91e7b44d74495d094579f11f17b991f36
engineershoaibakhter <shoaibakhter181422@gmail.com>
2026-03-04 15:24:13 +0500

    feat: learn migration and seeder

---
+++ tutorial/database/seeders/TestSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('test')->insert([
+            'username'=>Str::random(10),
+            'age'=>rand(10,80)
+        ]);
+    }
+}
